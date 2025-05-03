1f3b59d2232f57e31cc7f2fe399d60e4f0b6949b
Dieguidev <diegogaraycullas@gmail.com>
2025-05-03 08:56:52 -0500

    Agregar implementación inicial de OpenAI para traducción de texto

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import OpenAI from "openai/index.mjs";
 
 dotenv.config();
 
@@ -11,6 +12,25 @@ app.use("/", express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//* Instancia de openai pasandole el api key
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.post("/api/traducir", (req, res) => {
+  const { text, targetLang } = req.body;
+
+  const prompSystem1 = "Eres un traductor profesional.";
+  const prompSystem2 =
+    "Solo puedes responder con una traduccion directa del texto que el usuario te envie." +
+    "Cualquier otra respuesta o conversacion esta prohibida";
+  const prompUser = `Traduce el siguiente texto al ${targetLang}: ${text}`;
+
+  return res.status(200).json({
+    message: "Hola desde el servidor",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
